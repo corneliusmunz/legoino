e21023183abbcfbe522efce0380ef43947732c8f
Cornelius Munz <cornelius.munz@gmx.de>
2020-10-09 00:42:05 +0200

    small docu changes

--- examples/DuploTrainBase/DuploTrainBase.ino
+++ examples/DuploTrainBase/DuploTrainBase.ino
@@ -2,6 +2,10 @@
  * A Legoino example to connect a Duplo Train hub and read out the color and speed 
  * sensor values 
  * 
+ * Just put your Train on the Rails and push it a littlebit forward or backward. 
+ * If you lift it from the track or hold it, it will stop. The color sensor readings 
+ * should be directly visible in the Hub LED and dependent on the color a sound 
+ * will be played.
  * 
  * (c) Copyright 2020 - Cornelius Munz
  * Released under MIT License
