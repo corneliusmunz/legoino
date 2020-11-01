397daa8539679b302767e1d4acf26068eb7570b0
Cornelius Munz <cornelius.munz@gmx.de>
2020-11-01 23:22:34 +0100

    Adapt DeviceInfo example to request Version values explicitly

--- examples/MoveHubDeviceInfo/MoveHubDeviceInfo.ino
+++ examples/MoveHubDeviceInfo/MoveHubDeviceInfo.ino
@@ -13,6 +13,12 @@
 // create a hub instance
 Lpf2Hub myMoveHub;
 
+bool isHwVersionAvailable = false;
+bool isFwVersionAvailable = false;
+bool isBatteryTypeAvailable = false;
+
+bool isInitialized = false;
+
 void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint8_t *pData)
 {
   Lpf2Hub *myHub = (Lpf2Hub *)hub;
@@ -52,6 +58,7 @@ void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint
   {
     Serial.print("BatteryType: ");
     Serial.println(myHub->parseBatteryType(pData), HEX);
+    isBatteryTypeAvailable=true;
     return;
   }
 
@@ -66,7 +73,7 @@ void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint
     Serial.print(version.Bugfix);
     Serial.print(" Build: ");
     Serial.println(version.Build);
-
+    isFwVersionAvailable = true;
     return;
   }
 
@@ -81,7 +88,7 @@ void hubPropertyChangeCallback(void *hub, HubPropertyReference hubProperty, uint
     Serial.print(version.Bugfix);
     Serial.print(" Build: ");
     Serial.println(version.Build);
-
+    isHwVersionAvailable = true;
     return;
   }
 }
@@ -132,35 +139,46 @@ void loop()
   if (myMoveHub.isConnecting())
   {
     myMoveHub.connectHub();
-    if (myMoveHub.isConnected())
+    if (myMoveHub.isConnected() && !isInitialized)
     {
       Serial.println("Connected to HUB");
       delay(50); //needed because otherwise the message is to fast after the connection procedure and the message will get lost
-      myMoveHub.activateHubPropertyUpdate(HubPropertyReference::FW_VERSION, hubPropertyChangeCallback);
-      delay(50);
-      myMoveHub.activateHubPropertyUpdate(HubPropertyReference::HW_VERSION, hubPropertyChangeCallback);
-      delay(50);
       myMoveHub.activateHubPropertyUpdate(HubPropertyReference::ADVERTISING_NAME, hubPropertyChangeCallback);
-      delay(50);
-      myMoveHub.activateHubPropertyUpdate(HubPropertyReference::BATTERY_TYPE, hubPropertyChangeCallback);
       delay(50);
       myMoveHub.activateHubPropertyUpdate(HubPropertyReference::BATTERY_VOLTAGE, hubPropertyChangeCallback);
       delay(50);
       myMoveHub.activateHubPropertyUpdate(HubPropertyReference::BUTTON, hubPropertyChangeCallback);
       delay(50);
       myMoveHub.activateHubPropertyUpdate(HubPropertyReference::RSSI, hubPropertyChangeCallback);
-
       delay(50);
       myMoveHub.activatePortDevice((byte)MoveHubPort::TILT, portValueChangeCallback);
       delay(50);
       myMoveHub.activatePortDevice((byte)MoveHubPort::CURRENT, portValueChangeCallback);
       delay(50);
       myMoveHub.activatePortDevice((byte)MoveHubPort::VOLTAGE, portValueChangeCallback);
+      isInitialized = true;
     }
     else
     {
       Serial.println("Failed to connect to HUB");
     }
+  }
+
+  // FW/HW version properties and battery type need a explicit request to get the values back because they does not change over time
+  if (myMoveHub.isConnected() && !isFwVersionAvailable)
+  {
+    myMoveHub.requestHubPropertyUpdate(HubPropertyReference::FW_VERSION, hubPropertyChangeCallback);
+    delay(100);
+  }
+  if (myMoveHub.isConnected() && !isHwVersionAvailable)
+  {
+    myMoveHub.requestHubPropertyUpdate(HubPropertyReference::HW_VERSION, hubPropertyChangeCallback);
+    delay(100);
+  }
+  if (myMoveHub.isConnected() && !isBatteryTypeAvailable)
+  {
+    myMoveHub.requestHubPropertyUpdate(HubPropertyReference::BATTERY_TYPE, hubPropertyChangeCallback);
+    delay(100);
   }
 
   // if connected, print out continously the hub property values
